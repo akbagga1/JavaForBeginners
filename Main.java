a74b57eb5ec65b6bb65f3a50553b2857a741f338
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-19 14:31:10 -0400

    remove hello, name. add age and temperature.

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 30;
+        int temperature = 20;
+        age = 35;
+        System.out.println(age);
+    }
+}
