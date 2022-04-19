e5ff1ab2f3c99d2bae56d6b439ffac6fba933ab6
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-19 14:58:46 -0400

    update.

--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 30;
-        int temperature = 20;
-        age = 35;
-        System.out.println(age);
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
